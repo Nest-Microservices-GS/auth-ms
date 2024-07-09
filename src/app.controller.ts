217fc8193d534bf937035eca894c3768280fea45
Edgar Rios Navarro <edgarrios@bcp.com.pe>
2024-07-09 11:53:45 -0500

    add controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
-@Controller()
+@Controller('auth')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post('register')
+  registerUser( @Body() registerUserDto: RegisterUserDto) {
+    return this.appService.getHello();
+  }
+
+  @Post('login')
+  loginUser( @Body() loginUserDto: LoginUserDto) {
+    return this.appService.getHello();
+  }
+
+  @Get('verify')
+  verifyToken() {
     return this.appService.getHello();
   }
 }
